8e1142f4e7e7dc8208eb3626b13cdcad7597cae4
David Warrington <davidmwarrington+github@gmail.com>
2022-05-18 22:46:54 +0100

    feat(preferences): allow user to set an application to open projects in

--- src/open-project.tsx
+++ src/open-project.tsx
@@ -1,11 +1,15 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { Action, ActionPanel, Application, getApplications, List, open } from "@raycast/api";
+import { Action, ActionPanel, Application, getPreferenceValues, List, open } from "@raycast/api";
 import { useEffect, useState } from "react";
 
 interface ProjectDirectory {
   name: string;
   path: string;
+}
+
+interface Preferences {
+  preferredApplication: Application;
 }
 
 function OpenProjectAction({ onOpen }: { onOpen: () => void }) {
@@ -44,16 +48,14 @@ function useDirs(baseDir: string) {
 
 export default function OpenProject() {
   const { directories, getDirectories, state } = useDirs("/");
+  const preferences = getPreferenceValues<Preferences>();
 
-  const [apps, setApps] = useState<Application[]>([]);
   useEffect(() => {
     getDirectories();
-    getApplications("/").then((applications) => setApps(applications));
   }, []);
 
   function openInEditor(projectPath: string) {
-    const code = apps.find((app) => app.name.toLowerCase().includes("code"));
-    open(projectPath, code);
+    open(projectPath, preferences.preferredApplication);
   }
 
   return (
