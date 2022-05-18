9206a2cd52ad266b6745f937aad2d86a08084fe6
David Warrington <davidmwarrington+github@gmail.com>
2022-05-18 22:38:54 +0100

    feat(command): allow projects to be opened in vs code

--- src/open-project.tsx
+++ src/open-project.tsx
@@ -1,20 +1,34 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import { List } from "@raycast/api";
+import { Action, ActionPanel, Application, getApplications, List, open } from "@raycast/api";
 import { useEffect, useState } from "react";
 
+interface ProjectDirectory {
+  name: string;
+  path: string;
+}
+
+function OpenProjectAction({ onOpen }: { onOpen: () => void }) {
+  return <Action title="Open project" onAction={onOpen} />;
+}
+
 function useDirs(baseDir: string) {
-  const [directories, setDirectories] = useState<string[]>([]);
+  const [directories, setDirectories] = useState<ProjectDirectory[]>([]);
   const [state, setState] = useState<"loading" | "idle">("loading");
 
   async function getDirectories() {
     const dir = await fs.readdir(baseDir);
 
     const dirs = await Promise.all(
-      dir.filter(async (child) => {
-        const stats = await fs.stat(path.join(baseDir, child));
-        return stats.isDirectory();
-      })
+      dir
+        .filter(async (child) => {
+          const stats = await fs.stat(path.join(baseDir, child));
+          return stats.isDirectory();
+        })
+        .map((name) => ({
+          name,
+          path: path.join(baseDir, name),
+        }))
     );
 
     setState("idle");
@@ -31,14 +45,31 @@ function useDirs(baseDir: string) {
 export default function OpenProject() {
   const { directories, getDirectories, state } = useDirs("/");
 
+  const [apps, setApps] = useState<Application[]>([]);
   useEffect(() => {
     getDirectories();
+    getApplications("/").then((applications) => setApps(applications));
   }, []);
+
+  function openInEditor(projectPath: string) {
+    const code = apps.find((app) => app.name.toLowerCase().includes("code"));
+    open(projectPath, code);
+  }
 
   return (
     <List isLoading={state === "loading"}>
       {directories.map((directory) => (
-        <List.Item title={directory} key={directory} />
+        <List.Item
+          title={directory.name}
+          key={directory.path}
+          actions={
+            <ActionPanel>
+              <ActionPanel.Section>
+                <OpenProjectAction onOpen={() => openInEditor(directory.path)} />
+              </ActionPanel.Section>
+            </ActionPanel>
+          }
+        />
       ))}
     </List>
   );
